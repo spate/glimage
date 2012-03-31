6734c959a14ebb18bcbb1ff25da0641d4633d4d4
James Helferty <windmist@gmail.com>
2012-03-31 17:28:20 -0400

    Fix RGB565 color conversion

--- color/color.go
+++ color/color.go
@@ -12,11 +12,11 @@ type RGB565 struct {
 }
 
 func (c RGB565) RGBA() (r, g, b, a uint32) {
-	r = uint32(c.RGB<<16) & 0xf800
+	r = uint32(c.RGB)<<0 & 0xf800
 	r |= r>>5 | r>>10 | r>>15
-	g = uint32(c.RGB<<21) & 0xfc00
+	g = uint32(c.RGB)<<5 & 0xfc00
 	g |= g>>6 | g>>12
-	b = uint32(c.RGB<<26) & 0xf800
+	b = uint32(c.RGB)<<11 & 0xf800
 	b |= b>>5 | b>>10 | b>>15
 	a = 0xffff
 	return r, g, b, a
@@ -49,9 +49,9 @@ func rgb565Model(c color.Color) color.Color {
 		return c
 	}
 	r, g, b, _ := c.RGBA()
-	rgb := uint16(r>>16) & 0xf800
-	rgb |= uint16(g>>21) & 0x07e0
-	rgb |= uint16(b>>26) & 0x001f
+	rgb := uint16(r>>0) & 0xf800
+	rgb |= uint16(g>>5) & 0x07e0
+	rgb |= uint16(b>>11) & 0x001f
 	return RGB565{rgb}
 }
 
